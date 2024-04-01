9aa0d59a431eb35f1d26138851ccb5a30637a747
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2024-04-01 17:25:06 +0300

    Update KeySystem.lua

--- FroZenHubV1/Loader/KeySystem.lua
+++ FroZenHubV1/Loader/KeySystem.lua
@@ -1,1 +1,19 @@
+local KeySystemUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/MaGiXxScripter0/keysystemv2api/master/ui/xrer_mstudio45.lua"))()
+KeySystemUI.New({
+    ApplicationName = "FroZenHub", -- Your Key System Application Name
+    Name = "FroZen Hub V1", -- Your Script name
+    Info = "to acces FroZen Hub you have to cpmplete the key system. this will help us earn money to maintain the script.", -- Info text in the GUI, keep empty for default text.
+    DiscordInvite = "https://dsc.gg/frozen-x", -- Optional.
+    AuthType = "ip" -- Can select verification with ClientId or IP ("clientid" or "ip")
+})
+repeat task.wait() until KeySystemUI.Finished() or KeySystemUI.Closed
+if KeySystemUI.Finished() and KeySystemUI.Closed == false then
+    print("Key verified, can load script")
+    Load_GUI()
+else
+    print("Player closed the GUI.")
+end
 
+function Load_GUI()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/tridentsyr/main/FroZenHubV1/Loader/LoaderUI.lua"))()
+end
